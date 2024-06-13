5a7f4a5a111e92d6b81f956c19b2d54cbba9a077
jogzfon <jogzfon@gmail.com>
2024-06-13 21:15:46 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-module.exports = {
-  publicPath: '/MyProfile/',
-};
+const { defineConfig } = require('@vue/cli-service');
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/MyProfile/'
+    : '/'
+});
